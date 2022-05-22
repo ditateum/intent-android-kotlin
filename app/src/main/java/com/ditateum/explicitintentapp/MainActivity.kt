17504a8727727b754641958a6a62fe24d7356f15
Dita Permata Putra <ditateum@gmail.com>
2022-05-22 20:49:42 +0700

    Pindah Activity dengan data memanfaatkan putExtra

--- app/src/main/java/com/ditateum/explicitintentapp/MainActivity.kt
+++ app/src/main/java/com/ditateum/explicitintentapp/MainActivity.kt
@@ -7,16 +7,27 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnMoveActivity : Button
+    private lateinit var btnMoveActivityWithData : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnMoveActivity = findViewById(R.id.btn_move_activity)
+        btnMoveActivityWithData = findViewById(R.id.btn_move_activity_data)
 
+        // Pindah Activity tanpa Data dengan Intent
         btnMoveActivity.setOnClickListener {
             val moveIntent = Intent(this, MoveActivity::class.java)
             startActivity(moveIntent)
+        }
+
+        // Pindah Acitivty dengan Data with Intent
+        btnMoveActivityWithData.setOnClickListener {
+            val moveWithDataIntent = Intent(this, MoveWithDataActivity::class.java)
+            moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Dita Permata Putra")
+            moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 27)
+            startActivity(moveWithDataIntent)
         }
     }
 }
---
+++ app/src/main/java/com/ditateum/explicitintentapp/MoveWithDataActivity.kt
@@ -0,0 +1,28 @@
+package com.ditateum.explicitintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MoveWithDataActivity : AppCompatActivity() {
+    private lateinit var tvDataReceived : TextView
+
+    companion object {
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_data)
+
+        tvDataReceived = findViewById(R.id.tv_data_received)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getIntExtra(EXTRA_AGE, 0)
+
+        val text = "Name: $name, Your age: $age"
+
+        tvDataReceived.text = text
+    }
+}

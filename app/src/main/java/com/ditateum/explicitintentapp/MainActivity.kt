a80eafd91917d8221f356d1393ed54ccdab1b364
Dita Permata Putra <ditateum@gmail.com>
2022-05-23 10:09:20 +0700

    intent implisit untuk dial phone

--- app/src/main/java/com/ditateum/explicitintentapp/MainActivity.kt
+++ app/src/main/java/com/ditateum/explicitintentapp/MainActivity.kt
@@ -1,22 +1,18 @@
 package com.ditateum.explicitintentapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.ditateum.explicitintentapp.model.Person
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnMoveActivity : Button
     private lateinit var btnMoveActivityWithData : Button
     private lateinit var btnMoveActivityWithObject : Button
-
-    val person = Person(
-        name="Dita Permata Putra",
-        age = 27,
-        city = "Blitar",
-        email = "ditateum@gmail.com"
-        )
+    private lateinit var btnDialNumber : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,29 +21,50 @@ class MainActivity : AppCompatActivity() {
         btnMoveActivity = findViewById(R.id.btn_move_activity)
         btnMoveActivityWithData = findViewById(R.id.btn_move_activity_data)
         btnMoveActivityWithObject = findViewById(R.id.btn_move_activity_object)
+        btnDialNumber = findViewById(R.id.btn_dial_number)
 
-        // Pindah Activity tanpa Data dengan Intent
-        btnMoveActivity.setOnClickListener {
-            val moveIntent = Intent(this, MoveActivity::class.java)
-            startActivity(moveIntent)
-        }
-
-        // Pindah Acitivty dengan Data with Intent
-        btnMoveActivityWithData.setOnClickListener {
-            val moveWithDataIntent = Intent(this, MoveWithDataActivity::class.java)
-            moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Dita Permata Putra")
-            moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 27)
-            startActivity(moveWithDataIntent)
-        }
+        btnMoveActivity.setOnClickListener(this)
+        btnMoveActivityWithData.setOnClickListener(this)
+        btnMoveActivityWithObject.setOnClickListener(this)
+        btnDialNumber.setOnClickListener(this)
 
 
+    }
 
-        // Pindah Acitivty dengan Object with Intent
-        btnMoveActivityWithObject.setOnClickListener {
-            val moveWithObjectIntent = Intent(this, MoveWithObjectActivity::class.java)
-            moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
-            startActivity(moveWithObjectIntent)
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_move_activity -> {
+                // Pindah Activity tanpa Data dengan Intent
+                val moveIntent = Intent(this, MoveActivity::class.java)
+                startActivity(moveIntent)
 
+            }
+            R.id.btn_move_activity_data -> {
+                // Pindah Acitivty dengan Data with Intent
+                val moveWithDataIntent = Intent(this, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Dita Permata Putra")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 27)
+                startActivity(moveWithDataIntent)
+            }
+            R.id.btn_move_activity_object -> {
+                // Pindah Acitivty dengan Object with Intent
+                val person = Person(
+                    name="Dita Permata Putra",
+                    age = 27,
+                    city = "Blitar",
+                    email = "ditateum@gmail.com"
+                )
+
+                val moveWithObjectIntent = Intent(this, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
+
+            }
+            R.id.btn_dial_number -> {
+                val phoneNumber = "081210841382"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
+            }
         }
     }
 }
